7d5faa0eb68fe42546466636aba2ed0458d366f2
GitanjaliCoder <43063242+GitanjaliBhalerao@users.noreply.github.com>
2024-02-15 09:21:05 +0530

    Update app.py

--- Simple Question Answering Application langchain/project/app.py
+++ Simple Question Answering Application langchain/project/app.py
@@ -1,13 +1,10 @@
-#Hello! It seems like you want to import the Streamlit library in Python. Streamlit is a powerful open-source framework used for building web applications with interactive data visualizations and machine learning models. To import Streamlit, you'll need to ensure that you have it installed in your Python environment.
-#Once you have Streamlit installed, you can import it into your Python script using the import statement,
-
 import streamlit as st
 
 from langchain.llms import OpenAI
 
 #When deployed on huggingface spaces, this values has to be passed using Variables & Secrets setting, as shown in the video :)
 import os
-os.environ["OPENAI_API_KEY"] = "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"
+os.environ["OPENAI_API_KEY"] = "YOUR_OPENAI_SECRET_KEY"
 
 #Function to return the response
 def load_answer(question):
